044ac5fec25cf97c7cd92b5359268d0d85e88c77
sarim135 <148966257+sarim135@users.noreply.github.com>
2024-04-21 16:47:22 +0500

    Add files via upload

---
+++ day02/printing_1_to_10_num.py
@@ -0,0 +1,17 @@
+# Writing a Python script to print the numbers from 1 to 10 using a loop.
+# Using a for loop
+for i in range(1, 11):
+    print(i)
+
+
+
+
+# Writing a Python script that asks the user for their age and prints whether it's an even or odd number.
+# Ask the user for their age
+age = int(input("Please enter your age: "))
+
+# Check if the age is even or odd
+if age % 2 == 0:
+    print("Your age is even.")
+else:
+    print("Your age is odd.")
