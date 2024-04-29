7837a533e884af92c2055a7cc34c6e6958858ad6
sarim135 <148966257+sarim135@users.noreply.github.com>
2024-04-29 11:29:58 -0700

    Add files via upload

---
+++ day09/swap.py
@@ -0,0 +1,13 @@
+# Swapping the values of two variables using tuple unpacking
+a = 10
+b = 20
+print("\nBefore swapping:")
+print("a =", a)
+print("b =", b)
+
+# Swapping using tuple unpacking
+a, b = b, a
+
+print("\nAfter swapping:")
+print("a =", a)
+print("b =", b)
---
+++ day09/tuple.py
@@ -0,0 +1,7 @@
+# Creating a tuple with numbers from 1 to 5
+numbers_tuple = (1, 2, 3, 4, 5)
+
+# Printing each element of the tuple using a loop
+print("Elements of the tuple:")
+for num in numbers_tuple:
+    print(num)
