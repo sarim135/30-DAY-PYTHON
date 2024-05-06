f810cb2f58cf6b2f5cf774b299d83a1f677ac8d8
sarim135 <148966257+sarim135@users.noreply.github.com>
2024-05-06 07:21:46 -0700

    Add files via upload

---
+++ day13/dict.py
@@ -0,0 +1,5 @@
+# Generate the dictionary where keys are numbers between 1 and 20 (both included) and values are square of keys
+square_dict = {num: num ** 2 for num in range(1, 21)}
+
+print("Dictionary with keys as numbers between 1 and 20 and values as square of keys:")
+print(square_dict)
---
+++ day13/merg_dict.py
@@ -0,0 +1,9 @@
+# Define two dictionaries
+dict1 = {'a': 1, 'b': 2, 'c': 3}
+dict2 = {'d': 4, 'e': 5, 'f': 6}
+
+# Merge the dictionaries
+merged_dict = {**dict1, **dict2}
+
+print("Merged dictionary:")
+print(merged_dict)
